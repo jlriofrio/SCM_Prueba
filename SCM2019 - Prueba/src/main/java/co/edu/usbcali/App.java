ebf58e47c7566bed9f8d632866bc4e827e1ceae9
Jose Luis Riofrio <jlriofrio@gmail.com>
2019-06-13 13:21:43 -0500

    first commit

---
+++ SCM2019 - Prueba/src/main/java/co/edu/usbcali/App.java
@@ -0,0 +1,9 @@
+package co.edu.usbcali;
+
+public class App {
+	
+	public static void main(String[] args) {
+		System.out.println("Prueban de SCM");
+	}
+
+}
---
+++ SCM2019 - Prueba/src/test/java/co/edu/usbcali/AppTest.java
@@ -0,0 +1,19 @@
+package co.edu.usbcali;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class AppTest {
+
+	@Test
+	public void shouldAnswerWithTrue() {
+		assertTrue(true);
+	}
+
+	@Test
+	public void validacionNumerica() {
+		assertTrue(!false);
+	}
+
+}
